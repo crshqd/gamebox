e8b5ac2e0afea13f29bcb63677c619877e3b688d
Some random dude <125770478+crshqd@users.noreply.github.com>
2024-09-23 23:58:38 -0400

    live

--- games.js
+++ games.js
@@ -1,3 +1,3 @@
 const games = [
-  ["Game Site 1", "https://rawcdn.githack.com/crshqd/3kh0/ad011561c17b18afca7d663e7dbc1ecfaa609f71/"]
+  ["Game Site 1", "https://raw.githack.com/crshqd/3kh0/main/games.js"]
 ]
