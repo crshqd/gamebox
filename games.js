8c0d86becf23348d4d6df1593b3c525315ece94c
Some random dude <125770478+crshqd@users.noreply.github.com>
2024-09-25 22:29:25 -0400

    Update games.js

--- games.js
+++ games.js
@@ -1,3 +1,3 @@
 const games = [
-  ["3kh0 Game Site", "https://rawcdn.githack.com/crshqd/3kh0/0fbbb8ba9a21e91619ca470e54e94b21c27f3b62/games.js"]
+  ["3kh0 Game Site", "https://i-am-definitely-not.strangled.net/games.js"]
 ]
