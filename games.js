6069d1359c95f2fd8042820658463eadff6734d5
Some random dude <125770478+crshqd@users.noreply.github.com>
2024-10-20 16:14:15 -0400

    aaaa

--- games.js
+++ games.js
@@ -1,4 +1,4 @@
 const games = [
   ["3kh0 Game Site", "https://i-am-definitely-not.strangled.net/games.js"],
-  ["3kh0 School Site", "https://strangleedonvines.us.to/games.js"]
+  ["3kh0 School Site", "https://omgthisfiltersucks.us.to/games.js"]
 ]
